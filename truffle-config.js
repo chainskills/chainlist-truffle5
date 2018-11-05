a4b04a64a887b272efc54ecc698ceb6862b1c88e
eloudsa <said@noratek.net>
2018-11-05 23:58:11 +0100

    Config for geth

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,18 @@
 module.exports = {
-     // See <http://truffleframework.com/docs/advanced/configuration>
-     // to customize your Truffle configuration!
-     networks: {
-          ganache: {
-               host: "localhost",
-               port: 7545,
-               network_id: "*", // Match any network id
-               websockets: true
-          }
-     }
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    networks: {
+        ganache: {
+            host: "localhost",
+            port: 7545,
+            network_id: "*", 
+            websockets: true
+        },
+        chainskills: {
+            host: "localhost",
+            port: 8546,
+            network_id: "4224", 
+            websockets: true
+        }
+    }
 };
