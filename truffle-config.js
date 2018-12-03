37d0a84e913d839dd09ea75e71b4300225f15d9f
Said Eloudrhiri <info@noratek.net>
2018-12-04 00:16:33 +0100

    Configure the Solidity compiler

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,24 @@
 module.exports = {
-     // See <http://truffleframework.com/docs/advanced/configuration>
-     // to customize your Truffle configuration!
-     networks: {
-          ganache: {
-               host: "localhost",
-               port: 7545,
-               network_id: "*", // Match any network id
-               websockets: true
-          }
-     }
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    networks: {
+        ganache: {
+            host: "localhost",
+            port: 7545,
+            network_id: "*", // Match any network id
+            websockets: true
+        }
+    },
+    // Configure your compilers
+    compilers: {
+        solc: {
+            version: "0.4.25",    // Fetch exact version from solc-bin (default: truffle's version)
+            settings: {          // See the solidity docs for advice about optimization and evmVersion
+                optimizer: {
+                    enabled: false,
+                    runs: 200
+                }
+            }
+        }
+    }
 };
