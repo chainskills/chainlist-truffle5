fc0673be664e83093bbfc255eeef2888f341a307
Said Eloudrhiri <info@noratek.net>
2018-12-05 01:58:52 +0100

    enable optimizer

--- truffle-config.js
+++ truffle-config.js
@@ -14,7 +14,7 @@ module.exports = {
             version: "0.4.25",    // Fetch exact version from solc-bin (default: truffle's version)
             settings: {          // See the solidity docs for advice about optimization and evmVersion
                 optimizer: {
-                    enabled: false,
+                    enabled: true,
                     runs: 200
                 }
             }
