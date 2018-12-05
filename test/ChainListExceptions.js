2c2da9d169099817e4933d122f16d9db9019d384
Said Eloudrhiri <info@noratek.net>
2018-12-06 00:03:56 +0100

    Implement tests (happy and exceptions)

---
+++ test/ChainListExceptions.js
@@ -0,0 +1,139 @@
+const ChainList = artifacts.require("ChainList");
+
+// test suite
+contract("ChainList", accounts => {
+    let chainListInstance;
+    const seller = accounts[1];
+    const buyer1 = accounts[2];
+    const buyer2 = accounts[3];
+    const articleName = "article 1";
+    const articleDescription = "Description for article 1";
+    const articlePrice = 10;
+
+    before("setup contract for each test", async () => {
+        chainListInstance = await ChainList.deployed();
+    });
+
+    // no article for sale yet
+    it("should throw an exception if you try to buy an article when there is no article for sale yet", async () => {
+        try {
+            await chainListInstance
+                .buyArticle({
+                    from: buyer1,
+                    value: web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether")
+                });
+
+            // we should never reach this step
+            assert.fail();
+        } catch(e){
+            assert(true);
+        }
+
+        const article = await chainListInstance.getArticle();
+
+        assert.equal(article._seller, 0x0, "seller must be empty");
+        assert.equal(article._buyer, 0x0, "buyer must be empty");
+        assert.equal(article._name, "", "article name must be empty");
+        assert.equal(article._description, "", "description must be empty");
+        assert.equal(web3.utils.toBN(article._price), 0, "article price must be zero");
+    });
+
+    // buying an article you are selling
+    it("should throw an exception if you try to buy your own article", async () => {
+        // sell an article
+        await chainListInstance.sellArticle(
+            articleName,
+            articleDescription,
+            web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether"),
+            {from: seller}
+        );
+
+        try {
+            await chainListInstance.methods
+                .buyArticle({
+                    from: seller,
+                    value: web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether")
+                });
+
+            // we should never reach this step
+            assert.fail();
+        } catch(e){
+            assert(true);
+        }
+
+        const article = await chainListInstance.getArticle();
+
+        assert.equal(article._seller, seller, "seller must be " + seller);
+        assert.equal(article._buyer, 0x0, "buyer must be empty");
+        assert.equal(article._name, articleName, "article name must be " + articleName);
+        assert.equal(article._description, articleDescription, "description must be " + articleDescription);
+        assert.equal(
+            web3.utils.toBN(article._price),
+            web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether"),
+            "article price must be " + web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether")
+        );
+    });
+
+    // incorrect price
+    it("should throw an exception if you try to buy an article for a value different from its price", async () => {
+        try {
+            await chainListInstance
+                .buyArticle({
+                    from: buyer1,
+                    value: web3.utils.toWei(web3.utils.toBN(articlePrice + 1).toString(), "ether")
+                });
+
+            // we should never reach this step
+            assert.fail();
+        } catch(e){
+            assert(true);
+        }
+
+        const article = await chainListInstance.getArticle();
+
+        assert.equal(article._seller, seller, "seller must be " + seller);
+        assert.equal(article._buyer, 0x0, "buyer must be empty");
+        assert.equal(article._name, articleName, "article name must be " + articleName);
+        assert.equal(article._description, articleDescription, "description must be " + articleDescription);
+        assert.equal(
+            web3.utils.toBN(article._price),
+            web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether"),
+            "article price must be " + web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether")
+        );
+    });
+
+    // article has already been sold
+    it("should throw an exception if you try to buy an article that has already been sold", async () => {
+        await chainListInstance
+            .buyArticle({
+                from: buyer1,
+                value: web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether")
+            });
+
+        try {
+            await chainListInstance
+                .buyArticle({
+                    from: buyer2,
+                    value: web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether")
+                })
+
+            // we should never reach this step
+            assert.fail();
+        } catch(e){
+            assert(true);
+        }
+
+        const article = await chainListInstance.getArticle();
+
+        assert.equal(article._seller, seller, "seller must be " + seller);
+        assert.equal(article._buyer, buyer1, "buyer must be " + buyer1);
+        assert.equal(article._name, articleName, "article name must be " + articleName);
+        assert.equal(article._description, articleDescription, "description must be " + articleDescription);
+        assert.equal(
+            web3.utils.toBN(article._price),
+            web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether"),
+            "article price must be " + web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether")
+        );
+    });
+
+});
--- test/ChainListHappyPath.js
+++ test/ChainListHappyPath.js
@@ -4,9 +4,12 @@ const ChainList = artifacts.require("ChainList");
 contract("ChainList", accounts => {
     let chainListInstance;
     const seller = accounts[1];
+    const buyer = accounts[2];
     const articleName = "article 1";
     const articleDescription = "Description for article 1";
     const articlePrice = 10;
+    let sellerBalanceBeforeSale, sellerBalanceAfterSale;
+    let buyerBalanceBeforeSale, buyerBalanceAfterSale;
 
     before("setup contract for each test", async () => {
         chainListInstance = await ChainList.deployed();
@@ -16,6 +19,7 @@ contract("ChainList", accounts => {
         const article = await chainListInstance.getArticle();
 
         assert.equal(article._seller, 0x0, "seller must be empty");
+        assert.equal(article._buyer, 0x0, "buyer must be empty");
         assert.equal(article._name, "", "article name must be empty");
         assert.equal(article._description, "", "description must be empty");
         assert.equal(web3.utils.toBN(article._price), 0, "article price must be zero");
@@ -26,11 +30,12 @@ contract("ChainList", accounts => {
             articleName,
             articleDescription,
             web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether"),
-            { from: seller }
+            {from: seller}
         );
         const article = await chainListInstance.getArticle();
 
         assert.equal(article._seller, seller, "seller must be " + seller);
+        assert.equal(article._buyer, 0x0, "buyer must be empty");
         assert.equal(article._name, articleName, "article name must be " + articleName);
         assert.equal(article._description, articleDescription, "description must be " + articleDescription);
         assert.equal(
@@ -46,7 +51,7 @@ contract("ChainList", accounts => {
             articleName,
             articleDescription,
             web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether"),
-            { from: seller }
+            {from: seller}
         );
 
         assert.equal(receipt.logs.length, 1, "one event should have been triggered");
@@ -59,4 +64,43 @@ contract("ChainList", accounts => {
             "event article price must be " + articlePrice
         );
     });
+
+    it("should let us buy the first article", async () => {
+        // record balances of seller and buyer before the sale
+        let balance = await web3.eth.getBalance(seller)
+        sellerBalanceBeforeSale = web3.utils.fromWei(balance, "ether")
+
+        balance = await web3.eth.getBalance(buyer)
+        buyerBalanceBeforeSale = web3.utils.fromWei(balance, "ether")
+
+        // buy the article
+        await chainListInstance
+            .buyArticle({
+                from: buyer,
+                value: web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether")
+            });
+
+        // record balances of seller and buyer after the sale
+        balance = await web3.eth.getBalance(seller)
+        sellerBalanceAfterSale = web3.utils.fromWei(balance, "ether")
+
+        balance = await web3.eth.getBalance(buyer)
+        buyerBalanceAfterSale = web3.utils.fromWei(balance, "ether")
+
+        const article = await chainListInstance.getArticle();
+
+        assert.equal(article._seller, seller, "seller must be " + seller);
+        assert.equal(article._buyer, buyer, "buyer must be " + buyer);
+        assert.equal(article._name, articleName, "article name must be " + articleName);
+        assert.equal(article._description, articleDescription, "description must be " + articleDescription);
+        assert.equal(
+            web3.utils.toBN(article._price),
+            web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether"),
+            "article price must be " + web3.utils.toWei(web3.utils.toBN(articlePrice).toString(), "ether")
+        );
+
+        // check the effect of buy on balances of buyer and seller, accounting for gas
+        assert(parseFloat(sellerBalanceAfterSale) === (parseFloat(sellerBalanceBeforeSale) + parseFloat(articlePrice)), "seller should have earned " + articlePrice + " ETH");
+        assert(parseFloat(buyerBalanceAfterSale) <= (parseFloat(buyerBalanceBeforeSale) - parseFloat(articlePrice)), "buyer should have spent " + articlePrice + " ETH");
+    })
 });
