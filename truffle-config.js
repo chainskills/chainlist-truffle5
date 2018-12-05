95c29cd0b39b35efad006e29822f2a66fa6c3683
Said Eloudrhiri <info@noratek.net>
2018-12-05 02:17:20 +0100

    support solc 0.5

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,7 @@ module.exports = {
         ganache: {
             host: "localhost",
             port: 7545,
-            network_id: "*", 
+            network_id: "*",
             websockets: true
         },
         chainskills: {
@@ -17,10 +17,9 @@ module.exports = {
     // Configure your compilers
     compilers: {
         solc: {
-            version: "0.4.25",    // Fetch exact version from solc-bin (default: truffle's version)
             settings: {          // See the solidity docs for advice about optimization and evmVersion
                 optimizer: {
-                    enabled: false,
+                    enabled: true,
                     runs: 200
                 }
             }
